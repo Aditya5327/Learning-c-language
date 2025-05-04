a58f5c1272cfbcdb6f49193a6a49a29c2895e740
Aditya5327 <jainaditya16106@gmail.com>
2025-05-04 14:52:18 +0530

    in v1 learned about variables,data types,basic operators and functions like printf and scanf

---
+++ video_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    int d = 4;
+    char e = 'e';
+    float random_decimal1 = 4.435234;
+    double random_decimal2 = 4.435234567;
+    printf("%f\n", random_decimal1);
+    printf("%.16lf\n", random_decimal2);
+    printf("%d\n", 9 + 6);
+    printf("give me value of a,b,c\n");
+    scanf("%d%d%d", &a, &b, &c);
+}
