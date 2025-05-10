60f2d52ee5c3e9e4152b09a67a8d434d94f14d18
Aditya5327 <jainaditya16106@gmail.com>
2025-05-10 16:53:58 +0530

    in v11 learned about goto statement.

---
+++ video_11.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, num;
+    for (i = 0; i < 8; i++)
+    {
+        printf("%d", i);
+        for (j = 0; j < 8; j++)
+        {
+            printf("enter the number,enter 0 to exist\n");
+            scanf("%d", &num);
+            if (num == 0)
+            {
+                goto end;
+            }
+        }
+    }
+end:
+    return 0;
+}
